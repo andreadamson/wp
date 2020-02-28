c9c628e0ca6961bed96711154f629aae8587f432
Andre <andre.adamson@khk.ee>
2020-02-28 12:26:36 +0200

    modified theme `drakqen`

---
+++ wp-content/themes/drakqen/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+ <head>
+	<?php wp_head(); ?>
+ </head>
+
+ <body <?php body_class(); ?>>
+    <header>
+    
+    </header>
+    
+    <main>
+        <section>
+            
+        </section>
+        <section>
+            
+        </section>
+        <section>
+            
+        </section>
+        <section>
+            
+        </section>
+    </main>
+ </body>
+</html>
